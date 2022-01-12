7aa0c089673b324d1243f1ed52f56c34eec75b09
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2022-01-11 22:16:35 -0300

    Done first exercise of useState and added some extra notebook infos

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -21,7 +21,17 @@ const UseState = (props) => {
                     <button className="btn"
                     onClick={() => setCounter(counter + 1)}>+1</button>
                 </div>
-
+                <div className="notes">
+                    <h2>Caderno de Anotações</h2>
+                    <p>Antigamente, não existia como criar um estado dentro de um componente funcional. Agora isso existe com o <strong>useState</strong>. Nossa aplicação é um <strong>contador simples</strong>, considerado o "Olá mundo" do useState.</p>
+                    <p>Uma observação importante aqui é <strong>entender bem a arrow-function</strong> dentro dos botões +1 e -1:</p>
+                    <code>
+                        () => setCounter(counter - 1)
+                        () => setCounter(counter - 1)
+                    </code>
+                    <p>As expressões acima significam o seguinte: quando eu clicar no botão "()" então "=>" eu vou setar o counter colocando como parâmetro o "counter - 1" ou "counter + 1": isso só vai acontecer quando eu <strong>clicar</strong>.</p>
+                    <p>Pelo React, eu não altero o estado diretamente - eu altero o estado por meio do setcounter: primeiro eu altero o dado e a interface vai <strong>reagir</strong> a essa mudança.</p>
+                </div>
 
             </div>
 
