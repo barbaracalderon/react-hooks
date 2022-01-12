2b9059e7b709f6923359051ab3e70e19111ffda9
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2022-01-12 00:54:38 -0300

    Added controlled input to useEffect exercise

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
 
 const UseEffect = (props) => {
+
+    const [number, setNumber] = useState(1)
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <SectionTitle title="#03 - Input number com fatorial" />
+            <input type="number" className="input"
+            onChange={ e => setNumber(e.target.value)} />
         </div>
     )
 }
