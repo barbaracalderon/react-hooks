5de46fc8f8c80483112bff0caac0b66d16abd403
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2022-01-15 18:20:20 -0300

    Created UseCallback.jsx and UseCallbackButtons.jsx

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import UseCallbackButtons from './UseCallbackButtons'
 
 const UseCallback = (props) => {
+    const [count, setCount] = useState(0)
+
+    const inc = useCallback(function (delta) {
+        setCount(current => current + delta)
+    }, [setCount])
+
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
+                subtitle="Retorna uma função memorizada!"
             />
+            <SectionTitle title="#08 - Exercício" />
+            <div className="center">
+                <span className="text"> { count } </span>
+                <div>
+                    <UseCallbackButtons inc={inc} />
+                </div>
+            </div>
         </div>
     )
 }
 
-export default UseCallback
+export default React.memo(UseCallback)
